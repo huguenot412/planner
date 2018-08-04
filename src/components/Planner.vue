10c451197c4715dd633f4670579ab697c4c8dac2
Chris Snow <cgsnow@live.com>
2018-08-04 11:27:45 -0600

    setup

---
+++ src/components/Planner.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="week">
+    <div class="day"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Planner',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
